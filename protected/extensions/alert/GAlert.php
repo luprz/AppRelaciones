4d969d4930bed79ddd49dccf401a216113f494cb
Luis <luis@localhost>
2013-05-13 10:15:14 -0430

    Iniciando rama desarrollo (#1)

--- protected/extensions/alert/GAlert.php
+++ protected/extensions/alert/GAlert.php
@@ -15,4 +15,5 @@ class GAlert extends CWidget
 	
 	}
 	*/
+}
 ?>
--- protected/views/usuarios/admin.php
+++ protected/views/usuarios/admin.php
@@ -41,7 +41,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 </div><!-- search-form -->
 <?php 
 
-$this->widget('ext.alert.GAlert');
+//$this->widget('ext.alert.GAlert');
 
 //echo Chtml::link('Pincha aqui', '#', array('onclick'=>'alert("Hoooooooola! :D")'));
 
